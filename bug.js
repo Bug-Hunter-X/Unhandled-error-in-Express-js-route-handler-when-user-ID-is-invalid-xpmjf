6033cf53c276dca461b8d1d15b9884fdf346b75e
BugHunter-X <askzenai@gmail.com>
2024-12-30 22:14:08 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+app.get('/users/:id', (req, res) => {
+  const userId = req.params.id;
+  // Missing error handling if userId is not a number
+  const user = users.find(user => user.id === parseInt(userId));
+  if (user) {
+    res.json(user);
+  } else {
+    res.status(404).send('User not found');
+  }
+});
+
+// ... other routes ...
+
+app.listen(3000, () => console.log('Server listening on port 3000'));
